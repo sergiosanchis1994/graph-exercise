63a2786c24236931333ca305465c08a5c3a0ccc1
Sergio <sergiosanchis1994@gmail.com>
2019-09-23 18:24:06 +0200

    Add options to do and some error messages

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,41 @@
+import Utils.IOUtils;
 
+import java.io.IOException;
+
+import static Utils.IOUtils.getUserInput;
+import static Utils.IOUtils.printErrorOption;
+import static Utils.IOUtils.printIOException;
+import static Utils.IOUtils.printMain;
+import static Utils.IOUtils.printNumberFormatException;
+import static Utils.IOUtils.printUsage;
 
 public class Application {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        runMain();
+    }
 
+    private static void runMain(){
+        try {
+            printMain();
+            int userOption = Integer.parseInt(getUserInput());
+            switch (userOption){
+                case 1:
+                    break;
+                case 2:
+                    printUsage();
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    printErrorOption();
+            }
+        } catch (IOException e) {
+            printIOException();
+        } catch (NumberFormatException e) {
+            printNumberFormatException();
+        } finally {
+           runMain();
+        }
     }
 }
--- src/main/java/Utils/IOUtils.java
+++ src/main/java/Utils/IOUtils.java
@@ -1,16 +1,52 @@
 package Utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class IOUtils {
 
-    private static final String USAGE_TITLE = "## USAGE ##";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String MAIN_TEXT = "Please, select option desired:\n 1. Introduce Graph\n 2. Show usage\n 0. Exit";
+    private static final String USAGE_TITLE = "\n## USAGE ##";
     private static final String WONDER = "Given a non-directed graph, all connected components will be printed line by line.";
     private static final String INPUT_INSTRUCTIONS = "To enter a graph, you only have to enter the numbers in pairs that will form an edge. For example, 011234";
     private static final String OUTPUT_INSTRUCTIONS = "In the output, all related nodes will be shown in separate rows. For example:\n012\n34";
+    private static final String ERROR_OPTION = "This is not a valid option.";
+    private static final String NUMBER_FORMAT_EXCEPTION = "Input must be a number.";
+    private static final String IO_EXCEPTION = "Input throws an unexpected error.";
+
+
+    public static void printMain(){
+        System.out.println(MAIN_TEXT);
+    }
 
     public static void printUsage(){
         System.out.println(USAGE_TITLE);
         System.out.println(WONDER);
         System.out.println(INPUT_INSTRUCTIONS);
         System.out.println(OUTPUT_INSTRUCTIONS);
+    }
+
+    private static void printErrorMessage(String message){
+        System.out.println(ANSI_RED + message + ANSI_RESET);
+    }
+
+    public static void printErrorOption(){
+        printErrorMessage(ERROR_OPTION);
+    }
+
+    public static void printNumberFormatException(){
+        printErrorMessage(NUMBER_FORMAT_EXCEPTION);
+    }
+
+    public static void printIOException(){
+        printErrorMessage(IO_EXCEPTION);
+    }
+
+    public static String getUserInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
     }
 }
