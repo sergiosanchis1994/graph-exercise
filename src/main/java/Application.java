a72bb1a6ef28bd84eb1793ff7d0b61b5a366b676
Sergio <sergiosanchis1994@gmail.com>
2019-09-24 18:00:07 +0200

    Update main application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,12 @@
-import Utils.IOUtils;
+import Graph.Graph;
 
 import java.io.IOException;
 
 import static Utils.IOUtils.getUserInput;
+import static Utils.IOUtils.printErrorMessage;
 import static Utils.IOUtils.printErrorOption;
 import static Utils.IOUtils.printIOException;
+import static Utils.IOUtils.printInsertGraph;
 import static Utils.IOUtils.printMain;
 import static Utils.IOUtils.printNumberFormatException;
 import static Utils.IOUtils.printUsage;
@@ -15,12 +17,16 @@ public class Application {
         runMain();
     }
 
-    private static void runMain(){
+    private static void runMain() {
         try {
             printMain();
             int userOption = Integer.parseInt(getUserInput());
-            switch (userOption){
+            switch (userOption) {
                 case 1:
+                    printInsertGraph();
+                    String inputGraph = getUserInput();
+                    Graph graph = new Graph(inputGraph);
+                    graph.printGraph();
                     break;
                 case 2:
                     printUsage();
@@ -34,8 +40,10 @@ public class Application {
             printIOException();
         } catch (NumberFormatException e) {
             printNumberFormatException();
+        } catch (Exception e) {
+            printErrorMessage(e.getMessage());
         } finally {
-           runMain();
+            runMain();
         }
     }
 }
