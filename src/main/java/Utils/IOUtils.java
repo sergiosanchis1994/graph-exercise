65eb10184356bc80249245546603324f551fd30e
Sergio <sergiosanchis1994@gmail.com>
2019-09-24 18:10:31 +0200

    Update error messages and enable jar plugin

--- src/main/java/Utils/IOUtils.java
+++ src/main/java/Utils/IOUtils.java
@@ -6,12 +6,10 @@ import java.io.InputStreamReader;
 
 public class IOUtils {
 
-    private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_RESET = "\u001B[0m";
     private static final String MAIN_TEXT = "Please, select option desired:\n 1. Introduce Graph\n 2. Show usage\n 0. Exit";
     private static final String USAGE_TITLE = "\n## USAGE ##";
     private static final String WONDER = "Given a non-directed graph, all connected components will be printed line by line.";
-    private static final String INPUT_INSTRUCTIONS = "To enter a graph, you only have to enter the numbers in pairs that will form an edge. Empty values are not allowed. For example:\n2011234";
+    private static final String INPUT_INSTRUCTIONS = "To enter a graph, you only have to enter the numbers in pairs that will form an edge. Empty values are not allowed. For example:\n011234";
     private static final String OUTPUT_INSTRUCTIONS = "In the output, all related nodes will be shown in separate rows. For example:\n012\n34";
     private static final String ERROR_OPTION = "This is not a valid option.";
     private static final String NUMBER_FORMAT_EXCEPTION = "Input must be a number.";
@@ -49,7 +47,7 @@ public class IOUtils {
      * @param message which will be printed
      */
     public static void printErrorMessage(String message){
-        System.out.println(ANSI_RED + message + ANSI_RESET);
+        System.out.println("ERROR: " + message);
     }
 
     /**
