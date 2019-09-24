8807374bcc56dd76708d97a6778a723b6c896f94
Sergio <sergiosanchis1994@gmail.com>
2019-09-24 19:53:18 +0200

    ComponentTest WIP

---
+++ src/test/java/Graph/ComponentTest.java
@@ -0,0 +1,51 @@
+package Graph;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(JUnit4.class)
+public class ComponentTest {
+
+    private final static char NODE_VALUE = '1';
+
+    @Mock
+    private Node nodeMock;
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        when(nodeMock.getValue()).thenReturn(NODE_VALUE);
+    }
+
+    @Test
+    public void verify_addNode_addNewNodeWhenItNotExists(){
+        Component component = new Component();
+        assertTrue(component.getNodes().isEmpty());
+        component.addNode(nodeMock);
+        assertFalse(component.getNodes().isEmpty());
+        assertTrue(component.getNodes().contains(nodeMock));
+    }
+
+    @Test
+    public void verify_addNode_noAddNewNodeWhenItExists(){
+        Component component = new Component();
+        component.addNode(nodeMock);
+        assertFalse(component.getNodes().isEmpty());
+        assertEquals(1, component.getNodes().size());
+        component.addNode(nodeMock);
+        assertFalse(component.getNodes().isEmpty());
+        assertEquals(1, component.getNodes().size());
+    }
+}
